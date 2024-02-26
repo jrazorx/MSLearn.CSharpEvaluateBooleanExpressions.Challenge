d8b87eddd5979644386efd246267491f43a48a17
jra <julien.gault.50@gmail.com>
2024-02-26 15:13:05 +0100

    long solution

--- MSLearn.CSharpEvaluateBooleanExpressions.Challenge/Program.cs
+++ MSLearn.CSharpEvaluateBooleanExpressions.Challenge/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Random coin = new();
+
+// Let's say that 0 = heads and 1 = tails
+int coinFlip = coin.Next(2);
+string coinFlipResult = coinFlip == 0 ? "heads" : "tails";
+
+Console.WriteLine(coinFlipResult);
